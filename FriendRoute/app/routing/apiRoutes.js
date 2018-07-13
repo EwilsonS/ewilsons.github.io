8974f9204f7508713ecfc3c1b14a102b24c70bb9
Evan <ewilsons@aol.com>
2018-07-12 23:50:38 -0400

    api

---
+++ FriendRoute/app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+var friends = require("../data/friends")
+
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friends)
+    })
+
+    app.post("/api/friends", function (req, res) {
+        // something
+        friends.push(req.body)
+        res.json(true)
+
+        console.log(friends)
+    })
+}
