c7300548c0146c92fe27abddc9306c26d137140b
unknown <ewilsons@aol.com>
2018-12-03 20:51:17 -0500

    cleanup

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
+  //third party animation
   AOS.init();
   
-   
   // Add smooth scrolling to all links
   $(".scroll").on('click', function (event) {
     // Make sure this.hash has a value before overriding default behavior
@@ -22,91 +22,4 @@ $(document).ready(function () {
       });
     } // End if
   });
-
-  // Global variables
-  // =============================
-  var ranNum;
-  var blueNum;
-  var redNum;
-  var whiteNum;
-  var yellowNum;
-  var crystalScore = 0;
-
-  var wins = 0;
-  var losses = 0;
-
-  // functions
-  // =================================
-
-  // lets generate all of the random numbers needed to begin and store 
-  // them in a function so they reset together when a new game begins
-  function startGame() {
-    ranNum = Math.floor(Math.random() * 121) + 19;
-    $("#main-random").html(ranNum);
-
-
-    blueNum = Math.floor(Math.random() * 12) + 1;
-    redNum = Math.floor(Math.random() * 12) + 1;
-    whiteNum = Math.floor(Math.random() * 12) + 1;
-    yellowNum = Math.floor(Math.random() * 12) + 1;
-    console.log(ranNum, blueNum, redNum, whiteNum, yellowNum);
-
-  }
-  // this function adds the crystal presses into one score
-  function gamePlay() {
-    $("#blue-crystal").click(function () {
-      $("#score").html(crystalScore += blueNum);
-      winLose();
-    });
-    $("#red-crystal").click(function () {
-      $("#score").html(crystalScore += redNum);
-      winLose();
-    });
-    $("#white-crystal").click(function () {
-      $("#score").html(crystalScore += whiteNum);
-      winLose();
-    });
-    $("#yellow-crystal").click(function () {
-      $("#score").html(crystalScore += yellowNum);
-      winLose();
-    });
-  }
-  function winLose() {
-    if (crystalScore === ranNum) {
-      wins++;
-      $("#wins").html(wins);
-      $("#score").text(0);
-      $("#score").text("WINNER!")
-      reset();
-    } else if (crystalScore > ranNum) {
-      losses++;
-      $("#losses").html(losses);
-      $("#score").text(0)
-      $("#score").text("LOSER!")
-      reset();
-
-    }
-  }
-  function reset() {
-    crystalScore = 0;
-
-    ranNum = Math.floor(Math.random() * 121) + 19;
-    $("#main-random").html(ranNum);
-    blueNum = Math.floor(Math.random() * 12) + 1;
-    redNum = Math.floor(Math.random() * 12) + 1;
-    whiteNum = Math.floor(Math.random() * 12) + 1;
-    yellowNum = Math.floor(Math.random() * 12) + 1;
-
-
-
-
-  }
-
-  // main process
-  // ====================================
-  startGame();
-  gamePlay();
-
-  console.log(wins);
-
 })
