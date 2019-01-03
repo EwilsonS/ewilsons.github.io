9844b9aa16af39c4fb5369a085d9af4750141e43
EwilsonS <ewilsons@aol.com>
2019-01-03 09:45:02 -0500

    modal config

--- assets/javascript/strava.js
+++ assets/javascript/strava.js
@@ -1,29 +1,49 @@
 $(document).ready(() => {
+	// Add smooth scrolling to all links
+	$(".scroll").on('click', function (event) {
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+			// Store hash
+			var hash = this.hash;
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 500, function () {
 
-    let queryUrl = `https://www.googleapis.com/blogger/v3/blogs/3943554857418853370/posts?key=AIzaSyA9RAPlHjpSqJPYQx5z80rBVNWaRK4M3us`
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		}
+	});
 
-    function getBlogPosts() {
-        let posts = [];
+	// Blogger grab
+	let queryUrl = `https://www.googleapis.com/blogger/v3/blogs/3943554857418853370/posts?key=AIzaSyA9RAPlHjpSqJPYQx5z80rBVNWaRK4M3us`
 
-        $.ajax({
-            url: queryUrl,
-            method: 'GET'
-        }).then(res => {
-            for (let i = 0; i < 4; i++) {
+	function getBlogPosts() {
+		let posts = [];
 
-                posts.push(res.items[i])
-                let blogCard = $('<div class="card p-2" id="blog-card">') 
-                blogCard.html(`
+		$.ajax({
+			url: queryUrl,
+			method: 'GET'
+		}).then(res => {
+			for (let i = 0; i < 4; i++) {
+
+				posts.push(res.items[i])
+				let blogCard = $('<div class="card p-2" id="blog-card">')
+				blogCard.html(`
                 <h5 class="text-center" id="blog-title"><a class="text-dark" href="${posts[i].url}">${posts[i].title}</a>
                 <br><small>${posts[i].published}</small></h5>
                 <p id="blog-content">${posts[i].content}</p>
                 </div>`
-                );
+				);
 
-                $('.blog-posts').append(blogCard);
-                    console.log(posts[0])
-            };
-        })
-    }
-    getBlogPosts();
+				$('.blog-posts').append(blogCard);
+				console.log(posts[i])
+			};
+		})
+	}
+	getBlogPosts();
 })
