4995e9459a5e162d5cbc3fb4e68f6afe41a688ae
EwilsonS <ewilsons@aol.com>
2019-01-04 15:51:47 -0500

    side navigation/ search bar

--- assets/javascript/strava.js
+++ assets/javascript/strava.js
@@ -1,30 +1,40 @@
 $(document).ready(() => {
+	// Search bar
+	$('.search-bar').on('submit', function(){
+		alert('hi');
+		$('.main-input').val('');
+
+	})
+
 	// Add smooth scrolling to all links
 	$(".scroll").on('click', function (event) {
 		// Make sure this.hash has a value before overriding default behavior
 		if (this.hash !== "") {
-			// Prevent default anchor click behavior
 			event.preventDefault();
-			// Store hash
 			var hash = this.hash;
-			// Using jQuery's animate() method to add smooth page scroll
-			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
 			$('html, body').animate({
 				scrollTop: $(hash).offset().top
 			}, 500, function () {
-
-				// Add hash (#) to URL when done scrolling (default click behavior)
 				window.location.hash = hash;
 			});
 		}
 	});
 
-	// Blogger grab
+	// show side nav afterscrolled past main nav
+	$(document).scroll(function() {
+		var y = $(this).scrollTop();
+		if (y > 505) {
+		  $('.side-nav').fadeIn();
+		} else {
+		  $('.side-nav').fadeOut();
+		}
+	  });
+
+	// Blogger API grab
 	let queryUrl = `https://www.googleapis.com/blogger/v3/blogs/3943554857418853370/posts?key=AIzaSyA9RAPlHjpSqJPYQx5z80rBVNWaRK4M3us`
 
 	function getBlogPosts() {
 		let posts = [];
-		
 		
 		$.ajax({
 			url: queryUrl,
